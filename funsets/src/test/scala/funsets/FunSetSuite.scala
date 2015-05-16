d8aa667aa24c9e60f26b16e8e7c58d69cca61827
gmoh <gmoh@yahoo.com>
2015-05-15 21:55:48 -0400

    implements FunSets functions and test cases

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -77,6 +77,17 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+
+    val even = (i: Int) => i % 2 == 0;
+    val odd = (i: Int) => i % 2 != 0;
+    val negative = (i: Int) => i < 0;
+    val positive = (i: Int) => i > 0;
+
+    val identity = (i: Int) => i
+    val one_more = (i: Int) => i + 1
+    val double = (i: Int) => 2 * i
+    val triple = (i: Int) => 3 * i
+    val negate = (i: Int) => -1 * i
   }
 
   /**
@@ -86,7 +97,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -97,16 +108,136 @@ class FunSetSuite extends FunSuite {
        * The string argument of "assert" is a message that is printed in case
        * the test fails. This helps identifying which assertion failed.
        */
-      assert(contains(s1, 1), "Singleton")
+      assert(contains(s1, 1), "Contains 1 and 1 only")
+      assert(!contains(s1, 2), "Does not contain 2")
+      assert(!contains(s1, 3), "Does not contain 3")
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("intersect") {
+    new TestSets {
+      val s = intersect(s1, s2)
+      assert(!contains(s, 1), "Intersect 1, 2 does not contain 1")
+      assert(!contains(s, 2), "Intersect 1, 2 does not contain 2")
+
+      val t = intersect(union(s1, s2), union(s1, s3))
+      assert(contains(t, 1), "Intersection contains 1")
+      assert(!contains(t, 2), "Intersection does not contain 2")
+      assert(!contains(t, 3), "Intersection does not contain 3")
+    }
+  }
+
+  test("diff") {
+    new TestSets {
+      val s = diff(s1, s2)
+      assert(contains(s, 1), "Diff 1, 2 does not contain 1")
+      assert(!contains(s, 2), "Diff 1, 2 does not contain 2")
+
+      val t = diff(union(s1, s2), union(s1, s3))
+      assert(!contains(t, 1), "Diff does not contain 1")
+      assert(contains(t, 2), "Diff contains 2")
+      assert(!contains(t, 3), "Diff does not contain 3")
+
+      val u = diff(s1, union(s2, s3))
+      assert(contains(u, 1), "Diff contains 1")
+      assert(!contains(u, 2), "Diff does not contain 2")
+      assert(!contains(u, 3), "Diff does not contain 3")
+
+      val v = diff(union(s2, s3), s1)
+      assert(!contains(v, 1), "Diff does not contain 1")
+      assert(contains(v, 2), "Diff contains 2")
+      assert(contains(v, 3), "Diff contains 3")
+    }
+  }
+
+  test("filter") {
+    new TestSets {
+      assert(contains(filter(s1, odd), 1), "1 is odd")
+      assert(!contains(filter(s1, even), 1), "1 is not even")
+
+      assert(contains(filter(s2, even), 2), "2 is odd")
+      assert(!contains(filter(s2, odd), 1), "2 is not odd")
+
+      val s = union(s1, union(s2, s3))
+      assert(!contains(filter(s, negative), 1), "1 is not negative")
+      assert(!contains(filter(s, negative), 2), "2 is not negative")
+      assert(!contains(filter(s, negative), 3), "3 is not negative")
+    }
+  }
+
+  test("forall") {
+    new TestSets {
+      assert(forall(s1, odd), "1 is odd")
+      assert(!forall(s1, even), "1 is not even")
+
+      assert(forall(s2, even), "2 is even")
+      assert(!forall(s2, odd), "2 is not odd")
+
+      val s = union(s1, s3)
+      assert(forall(s, odd), "1 and 3 are odd")
+      assert(!forall(s, even), "1 nor 3 is even")
+
+      val t = union(s1, s2)
+      assert(!forall(t, odd), "not all odd")
+      assert(!forall(t, even), "not all even")
+      assert(forall(t, positive), "all positive")
+      assert(!forall(t, negative), "not all negative")
+    }
+  }
+
+  test("exists") {
+    new TestSets {
+      assert(exists(s1, odd), "1 is odd")
+      assert(!exists(s1, even), "1 is not even")
+
+      assert(exists(s2, even), "2 is even")
+      assert(!exists(s2, odd), "2 is not odd")
+
+      val s = union(s1, s3)
+      assert(exists(s, odd), "1 and 3 are odd")
+      assert(!exists(s, even), "1 nor 3 is even")
+
+      val t = union(s1, s2)
+      assert(exists(t, odd), "1 is odd")
+      assert(exists(t, even), "2 is even")
+      assert(exists(t, positive), "all positive")
+      assert(!exists(t, negative), "not all negative")
+    }
+  }
+
+  test("map") {
+    new TestSets {
+      assert(contains(map(s1, identity), 1), "1")
+      assert(contains(map(s1, one_more), 2), "1 + 1 = 2")
+      assert(contains(map(s1, double), 2), "1 * 2 = 2")
+      assert(contains(map(s1, triple), 3), "1 * 3 = 3")
+      assert(contains(map(s1, negate), -1), "1 * -1 = -1")
+
+      val s = union(s1, s2)
+      assert(contains(map(s, identity), 1), "1")
+      assert(contains(map(s, identity), 2), "2")
+      assert(!contains(map(s, identity), 3), "not 3")
+      assert(contains(map(s, one_more), 2), "1 + 1 = 2")
+      assert(contains(map(s, one_more), 3), "2 + 1 = 3")
+      assert(!contains(map(s, one_more), 4), "no 3")
+      assert(contains(map(s, double), 2), "1 * 2 = 2")
+      assert(contains(map(s, double), 4), "2 * 2 = 4")
+      assert(!contains(map(s, double), 6), "no 3")
+      assert(contains(map(s, triple), 3), "1 * 3 = 3")
+      assert(contains(map(s, triple), 6), "2 * 3 = 6")
+      assert(!contains(map(s, triple), 9), "no 3")
+      assert(contains(map(s, negate), -1), "1 * -1 = -1")
+      assert(contains(map(s, negate), -2), "2 * -1 = -2")
+      assert(!contains(map(s, negate), -3), "no 3")
     }
   }
 }
